573e4423fc0c0d1433a312cc395f4c58d290a330
jesussantiagomanzano <jesussantiagomanzano@gmail.com>
2023-05-05 08:11:58 -0600

    adding root controller

---
+++ src/main/java/com/example/springbootwithmockito/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.example.springbootwithmockito.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello AWS";
+    }
+}
